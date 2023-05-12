5c2b4c1cf16c6beb03fed875362fb207e777726b
Petr Gaube <33907344+lodhart@users.noreply.github.com>
2023-05-12 14:55:16 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
+let hack = 0
+let losovani_celkem = 0
+let losovani_aktualni_pocet = 0
 let kostka = 0
+input.onButtonPressed(Button.A, function () {
+    hack = 1
+})
 basic.forever(function () {
+    losovani_celkem = 50
     if (input.buttonIsPressed(Button.B)) {
-        for (let index = 0; index < 20; index++) {
+        for (let index = 0; index < losovani_celkem; index++) {
+            losovani_aktualni_pocet += 1
             kostka = randint(1, 6)
+            if (hack == 1 && losovani_aktualni_pocet == losovani_celkem) {
+                kostka = 6
+                hack = 0
+            }
             if (kostka == 1) {
                 basic.showLeds(`
                         . . . . .
@@ -53,5 +65,6 @@ basic.forever(function () {
                     `, 10)
             }
         }
+        losovani_aktualni_pocet = 0
     }
 })
