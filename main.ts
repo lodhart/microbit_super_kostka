3ba38e299ee8a14d0f3c2897b68cd95861e78f6e
Petr Gaube <33907344+lodhart@users.noreply.github.com>
2023-05-12 15:02:11 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,11 +14,11 @@ let hack = 0
 let losuj = 0
 losuj = 0
 basic.forever(function () {
-    losovani_celkem = 50
+    losovani_celkem = 100
     if (input.buttonIsPressed(Button.B) || losuj == 1) {
         for (let index = 0; index < losovani_celkem; index++) {
             losovani_aktualni_pocet += 1
-            kostka = randint(1, 6)
+            kostka = randint(1, 5)
             if (hack == 1 && losovani_aktualni_pocet == losovani_celkem) {
                 kostka = 6
                 hack = 0
